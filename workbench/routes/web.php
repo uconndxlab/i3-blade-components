a2de543822fb5b6b830bdf157279d2606465c876
Brian Daley <brian@uconn.edu>
2026-04-29 20:29:05 -0400

    base css for powered-by

--- workbench/routes/web.php
+++ workbench/routes/web.php
@@ -9,6 +9,31 @@ Route::get('/i3-blade-components.css', function () {
         ->getFileName();
     $packageRoot = dirname($providerPath, 2);
 
+    // In workbench, inline source CSS imports so component style edits appear immediately.
+    $sourceEntry = $packageRoot.'/resources/css/i3-blade-components.css';
+
+    if (File::exists($sourceEntry)) {
+        $entryContents = File::get($sourceEntry);
+        $cssContents = preg_replace_callback(
+            '/@import\s+["\']\.\/components\/([^"\']+)["\'];/',
+            function (array $matches) use ($packageRoot): string {
+                $componentCssPath = $packageRoot.'/resources/css/components/'.$matches[1];
+
+                if (! File::exists($componentCssPath)) {
+                    return '';
+                }
+
+                return PHP_EOL.File::get($componentCssPath).PHP_EOL;
+            },
+            $entryContents,
+        );
+
+        return response($cssContents, 200, [
+            'Content-Type' => 'text/css; charset=UTF-8',
+            'Cache-Control' => 'no-store, no-cache, must-revalidate, max-age=0',
+        ]);
+    }
+
     $candidatePaths = [
         $packageRoot.'/resources/dist/i3-blade-components.css',
         public_path('vendor/blade-components/i3-blade-components.css'),
